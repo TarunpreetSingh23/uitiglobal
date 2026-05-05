f28c4ee84bb9f8b7a64b9600bf5d4e4faf0bde7d
TarunpreetSingh23 <tarunpreets29@gmail.com>
2026-05-05 19:11:42 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>Universal Institute, Block 5, Near Main Road,<br />,Amritsar – 75300</span>
+                <span>Universal Institute,Guru Arjan Dev Nagar,opposite Khalsa college for Women<br />,Amritsar – 143002</span>
               </div>
               <div className="flex items-center gap-2">
                 <svg className="w-4 h-4 text-cyan-500 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
